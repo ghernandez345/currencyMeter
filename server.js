5673a4cb3a3e0b854188f4929af591666b1152c0
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-10-31 16:41:10 +0000

    add simple currency data endpoint

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+var request = require('request');
 var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
 
+
+var DATA_ENDPOINT = 'https://widgister.herokuapp.com/challenge/frontend';
+
 // Proxy to data endpoint
 app.get('/currency', function(req, res) {
-  res.send()
+  request(DATA_ENDPOINT, function(err, response, body) {
+    if (err || response.statusCode !== 200) {
+      return res.send({error: 'There was a request error'});
+    }
+
+    return res.send(body);
+  });
 });
 
 app.listen(3000, function() {
