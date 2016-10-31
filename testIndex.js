2b91e8af7e225cd28ded0198c39fcb4381cc4ae1
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-10-31 17:04:18 +0000

    set up testing infrastructure

---
+++ testIndex.js
@@ -0,0 +1,13 @@
+// test/test_index.js
+
+// require all modules ending in "_test" from the
+// current directory and all subdirectories
+var testsContext = require.context(".", true, /spec$/);
+testsContext.keys().forEach(function(path) {
+    try {
+        testsContext(path);
+    } catch(err) {
+        console.error('[ERROR] WITH SPEC FILE: ', path);
+        console.error(err);
+    }
+});
