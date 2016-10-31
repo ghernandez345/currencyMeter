c6a6779db87e2b08a1ed7619facc4ed826867049
Gabe Hernandez <gabriel.hernandez@lab49.com>
2016-10-31 17:05:11 +0000

    add main entry js file

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Main app entry point.
+ */
+
+import * as React from 'react';
+import { render } from 'react-dom';
+// import styles from './mainStyles.css';
+
+render(
+  <div>TESTING THIS!</div>,
+  document.getElementById('app-container')
+);
